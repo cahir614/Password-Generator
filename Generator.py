0eeb72182eb5fa0a47af207c8cf2a171b6c6d674
cahir614 <cahiraep@protonmail.com>
2020-07-19 13:03:56 +0200

    comments

--- Generator.py
+++ Generator.py
@@ -1,11 +1,13 @@
+# imports
 import string
 import secrets
 
+# string module for letters,digits, punctuation
 def generate(len):
     password_characters = string.ascii_letters + string.digits + string.punctuation
     secure_password = ''.join(secrets.choice(password_characters) for i in range(len))
     return print(secure_password)
-
+# define a password length
 def main():
     len = int(input("Password length? "))
     return generate(len)
