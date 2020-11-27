6d8cecce26d2229e7a647746112f043946c3013d
Cahir614 <65228781+cahir614@users.noreply.github.com>
2020-11-28 00:32:16 +0100

    Update len.sh

--- len.sh
+++ len.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo Password length: 
 read length
-python3 /home/$USER/Git/PassGen/gen.py $length | xclip -selection c 
+python3 /home/$USER/Password-Generator/gen.py $length | xclip -selection c 
 		
